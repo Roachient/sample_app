da514afa6cc00eac7b9c223f1fa1dbfc7e757352
Rodney Arnett <rodney@rodneygarnett.com>
2011-11-29 14:39:49 -0600

    Done with user edit/update, index, and destroy actions- all tests are good

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -243,28 +243,56 @@ describe UsersController do
         flash[:success].should =~ /welcome to the sample app/i
       end
     end
-
     describe "failure" do
-
       before(:each) do
         @attr = { :name => "", :email => "", :password => "",
                   :password_confirmation => "" }
       end
-
       it "should not create a user" do
         lambda do
           post :create, :user => @attr
         end.should_not change(User, :count)
       end
-
       it "should have the right title" do
         post :create, :user => @attr
         response.should have_selector("title", :content => "Sign up")
       end
-
       it "should render the 'new' page" do
         post :create, :user => @attr
         response.should render_template('new')
+      end
+    end
+  end
+  describe "DELETE 'destroy'" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+    describe "as a non-signed-in user" do
+      it "should deny access" do
+        delete :destroy, :id => @user
+        response.should redirect_to(signin_path)
+      end
+    end
+    describe "as a non-admin user" do
+      it "should protect the page" do
+        test_sign_in(@user)
+        delete :destroy, :id => @user
+        response.should redirect_to(root_path)
+      end
+    end
+    describe "as an admin user" do
+      before(:each) do
+        admin = Factory(:user, :email => "admin@example.com", :admin => true)
+        test_sign_in(admin)
+      end
+      it "should destroy the user" do
+        lambda do
+          delete :destroy, :id => @user
+        end.should change(User, :count).by(-1)
+      end
+      it "should redirect to the users page" do
+        delete :destroy, :id => @user
+        response.should redirect_to(users_path)
       end
     end
   end
